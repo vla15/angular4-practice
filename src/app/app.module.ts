217cc4366cc95794ca29cd4300b7f4ec836a9066
Vincent La <vincela15@gmail.com>
2017-09-23 15:27:03 -0700

    creating hero detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
