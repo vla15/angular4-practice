7d68cbc6ca4bb03355370b7383b6151c61ffa246
Vincent La <vincela15@gmail.com>
2017-09-23 16:11:26 -0700

    built out separate hero data service.  Injected it into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './services/hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   selector: 'app-root',
@@ -29,11 +18,22 @@ const HEROES: Hero[] = [
   <hero-detail [hero]="selectedHero"></hero-detail>`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Tour of heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then((heroes) => {
+        this.heroes = heroes;
+      });
+
+  }
+  constructor(private heroService: HeroService) { }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './services/hero.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
