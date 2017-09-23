7cba956ceafa3999268b7861a149e43c6031058a
Vincent La <vincela15@gmail.com>
2017-09-23 15:47:58 -0700

    breaking out hero-detail component to render detailed hero view

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeroDetailComponent {
+  @Input() hero: Hero;
 }
