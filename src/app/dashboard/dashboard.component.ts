3b75223d6f0ff17b0d9244c9037c48a2447d8f50
Vincent La <vincela15@gmail.com>
2017-09-26 17:45:32 -0700

    rendering first four heroes on the dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../services/hero.service';
+import { Hero } from '../hero';
+
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  heroes: Hero[];
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then((heroes) => {
+        this.heroes = heroes.slice(0, 4);
+      })
   }
-
+  constructor(private heroService: HeroService) { }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
